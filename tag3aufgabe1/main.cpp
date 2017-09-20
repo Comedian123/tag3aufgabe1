bfd0e3533e652262294e31dfdff84d079f95d93c
Sebastian Middeke <batman@dyn-8588509630eec89a55009000.eduroam.ipv6.ruhr-uni-bochum.de>
2017-09-20 13:46:24 +0200

    Initial Commit

---
+++ tag3aufgabe1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tag3aufgabe1
+//
+//  Created by Sebastian Middeke on 20.09.17.
+//  Copyright © 2017 Sebastian Middeke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
